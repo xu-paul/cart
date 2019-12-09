b9c88f6fc5d090f326ac4cb7d47c8b0d118ddc34
北风 <442603489@qq.com>
2019-12-09 11:38:28 +0800

    Updates

---
+++ precache-manifest.5e16231cb0207d1998d13a4310d6c269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be67b0712291fcce13ff9ea777140ad0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d93f768ab8edac9ae451",
+    "url": "./static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "e947a248b32835bf932d",
+    "url": "./static/css/main.4f19e2ed.chunk.css"
+  },
+  {
+    "revision": "d93f768ab8edac9ae451",
+    "url": "./static/js/2.ee6dc262.chunk.js"
+  },
+  {
+    "revision": "e947a248b32835bf932d",
+    "url": "./static/js/main.368a42a0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
